680241894cc04eff981ef3286a27040bcec5eb0c
dmuslymov <dmuslymov@moysklad.ru>
2019-06-05 11:27:52 +0300

    Dir change

--- config.rb
+++ config.rb
@@ -18,7 +18,7 @@ set :markdown,
 set :css_dir, 'stylesheets'
 set :js_dir, 'javascripts'
 set :images_dir, 'images'
-set :build_dir, 'build/api'
+set :build_dir, 'build'
 set :fonts_dir, 'fonts'
 
 # Activate the syntax highlighter
